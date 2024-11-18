b7c95c9810e365075673978bda54e831402d5a78
jealou18 <molenillajealou@gmail.com>
2024-11-18 12:11:43 +0800

    Add files via upload

---
+++ jealou.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class jealou
+{
+	public static void main(String[] args) {
+	for (int i = 1; i <= 5000; ++i) {
+		System.out.println("MOLENILLA, JEA LOU M.");
+		}
+	}
+}
